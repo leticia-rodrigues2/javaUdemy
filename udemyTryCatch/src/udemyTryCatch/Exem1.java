5fbb405521a6b6190b38b1dcea8d3467a4bc3edf
leticia-rodrigues2 <leticiaarodrigues2@gmail.com>
2021-05-14 20:09:44 -0300

    tratamento de exceções exemplo try-catch

---
+++ udemyTryCatch/src/udemyTryCatch/Exem1.java
@@ -0,0 +1,29 @@
+package udemyTryCatch;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exem1 {
+	public static void main(String[] args) {
+	Scanner entrada = new Scanner(System.in);
+	System.out.println("# DIGITE A FRASE COM ESPAÇOS PARA CRIAR O VETOR #");
+	try
+	{
+		String[] vect = entrada.nextLine().split(" ");
+		System.out.println("Digite a posição do vetor que deseja ver ");
+		int position = entrada.nextInt();
+		System.out.println(vect[position]);
+	}
+	catch(ArrayIndexOutOfBoundsException e)
+	{ // quando nao tiver o vetor que vc digitou
+		System.out.println("Posição invalida!");
+	}
+	catch(InputMismatchException e)
+	{ // se digitar uma letra ou cracter errado ou seja que não seja um numero
+		System.out.println("Erro de entrada");
+	}System.out.println("Fim do programa");
+	
+	entrada.close();
+}
+
+}
